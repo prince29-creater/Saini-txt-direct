c72d329d2a8019b6ac98cb3c4adccc7362c681c2
prince29-creater <bhaduprince1@gmail.com>
2025-06-18 22:43:57 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,9 +3,9 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "xxxxxxxx"))
-API_HASH = environ.get("API_HASH", "xxxxxxxxxxxxxxxxxxxx")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "21716150"))
+API_HASH = environ.get("API_HASH", "63f3b7ddd0f6f8570dbd4fb8a24ae289")
+BOT_TOKEN = environ.get("BOT_TOKEN", "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpZCI6MTU0MTcyOTAxLCJvcmdJZCI6NTU3MywidHlwZSI6MSwibW9iaWxlIjoiOTE5MzQxNTI1ODI4IiwibmFtZSI6IkphbnZpIiwiZW1haWwiOiJ5am04emhraHZ5QG9ha29uLmNvbSIsImlzRmlyc3RMb2dpbiI6dHJ1ZSwiZGVmYXVsdExhbmd1YWdlIjoiRU4iLCJjb3VudHJ5Q29kZSI6IklOIiwiaXNJbnRlcm5hdGlvbmFsIjowLCJpc0RpeSI6dHJ1ZSwibG9naW5WaWEiOiJPdHAiLCJmaW5nZXJwcmludElkIjoiYzc5NDgxMDIyM2Q5NDM2Njk3ZDYyMjU0ZDUyMzBiMGYiLCJpYXQiOjE3NTAyNTY0OTAsImV4cCI6MTc1MDg2MTI5MH0.fzTlUwkmjRSfKeoW5fiON_3pjfAVNoSYvqgiEJZ0qnJB51yAQSole17GmZLlcpIb")
 OWNER = int(environ.get("OWNER", "5680454765"))
 CREDIT = "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎"
 LOG_CHANNEL = "-1002631537674" # log group me bot ko admin narur banaye full rights ke sath
